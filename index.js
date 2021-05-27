c91d14b9f815388cab98fbc5a922f6b8136d528e
Yuliia Korabelska <yuliia_korabelska@epam.com>
2021-05-27 17:01:37 -0500

    Add news API.

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 require('dotenv').config()
+const apiToken = process.env.BOT_TOKEN;
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWS_API_TOKEN);
+const port = process.env.PORT;
 const express = require('express');
 const app = express();
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const port = 80;
 const url = 'https://api.telegram.org/bot';
-const apiToken = process.env.BOT_TOKEN;
 // Configurations
 app.use(bodyParser.json());
 // Endpoints
 app.post('/', (req, res) => {
     const chatId = req.body.message.chat.id;
     const sentMessage = req.body.message.text;
-    const replyMessage = resolveMessage(sentMessage);
-    axios.post(`${url}${apiToken}/sendMessage`, {
-            chat_id: chatId,
-            text: replyMessage
-        })
-        .then((response) => {
-            res.status(200).send(response);
-        }).catch((error) => {
-            res.send(error);
-        });
-    res.send(req.body);
+    newsapi.v2.everything({
+        q: sentMessage,
+        language: 'en',
+        sources: 'bbc-news,the-verge,reuters',
+        sortBy: 'relevancy',
+        page: 1
+    }).then(resp => {
+        const firstUrl = 'Here are the latest news for ' + sentMessage + " : " + resp.articles[0].title + " \n" + resp.articles[0].url;
+        console.log(firstUrl);
+        axios.post(`${url}${apiToken}/sendMessage`, {
+                chat_id: chatId,
+                text: firstUrl
+            })
+            .then((response) => {
+                res.status(200).send(response);
+            }).catch((error) => {
+                console.log(error);
+            });
+        res.send(req.body);
+    }).catch((error) => {
+        axios.post(`${url}${apiToken}/sendMessage`, {
+                chat_id: chatId,
+                text: 'Nothing found 🤷‍♀️'
+            })
+            .then((response) => {
+                res.status(200).send(response);
+            }).catch((error) => {
+                console.log(error);
+            });
+    });
 });
 // Listening
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
-
-function resolveMessage(message) {
-    if (message.match(/hello/gi)) {
-        return 'hello back 👋';
-    }
-
-    if (message.match(/doge/gi)) {
-        return 'TO THE MOOON!!! 🚀🚀🚀';
-    }
-
-    return 'Ничего не понял, нюхай бебру';
-}
